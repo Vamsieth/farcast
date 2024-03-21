ea32c040b1dc2c2efb0fcc1c0fb1fa5032479fb2
Vamsieth <svamsi.tech@gmail.com>
2024-03-22 02:30:46 +0530

    fix:frames

--- src/app/api/frame/route.ts
+++ src/app/api/frame/route.ts
@@ -11,7 +11,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
       return new NextResponse(`<!DOCTYPE html><html><head>
     <title>This is frame 7</title>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmRp3YMQFLwdP1LZdsMfbMrBJqfhcXoKAb6ZYz5p38FHZV/7.jpeg" />
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmRp3YMQFLwdP1LZdsMfbMrBJqfhcXoKAb6ZYz5p38FHZV/7.jpeg?pinataGatewayToken=${process.env.NEXT_GATEWAY_KEY}" />
     <meta property="fc:frame:button:1" content="Visit diviswap.com" />
     <meta property="fc:frame:button:1:action" content="post_redirect" />
     <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end" />
